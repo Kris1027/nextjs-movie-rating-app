7951a5fb248b0c58f7066dec199f8408096aac16
Kris1027 <obarzanek.work@gmail.com>
2024-01-15 13:19:22 +0100

    Add clsx library and update button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,12 +1,18 @@
+import clsx from 'clsx';
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 type ButtonProps = {
   children: ReactNode;
+  isSelected?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ children, ...props }: ButtonProps) {
+export default function Button({
+  children,
+  isSelected,
+  ...props
+}: ButtonProps) {
   return (
-    <button className="bg-slate-400" {...props}>
+    <button className={clsx('p-2', { 'bg-blue-500': isSelected })} {...props}>
       {children}
     </button>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+import { useState } from 'react';
+import Button from './components/button';
+
+enum DisplayType {
+  Movies = 'movies',
+  TvShows = 'tvshows',
+}
+
 export default function Home() {
+  const [displayType, setDisplayType] = useState<DisplayType>(
+    DisplayType.Movies
+  );
+
   return (
     <main>
-      <h1>Home page</h1>
+      <div className="flex justify-center gap-1">
+        <Button
+          onClick={() => setDisplayType(DisplayType.Movies)}
+          isSelected={displayType === DisplayType.Movies}
+        >
+          Movies
+        </Button>
+        <Button
+          onClick={() => setDisplayType(DisplayType.TvShows)}
+          isSelected={displayType === DisplayType.TvShows}
+        >
+          Tv Shows
+        </Button>
+      </div>
     </main>
   );
 }
