470d74e40bbfc375e28f5d289f3609914ab754ac
Kris1027 <obarzanek.work@gmail.com>
2024-01-16 03:30:51 +0100

    build footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex bg-secondary items-center justify-center h-10 p-6">
+      <p>
+        © 2024 All rights reserved. Designed & crafted with love by Kris1027
+      </p>
+    </footer>
+  );
 }
