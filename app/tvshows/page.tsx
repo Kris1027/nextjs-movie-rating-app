20619f057b353c294dd41d407cb1178d34480510
Kris1027 <obarzanek.work@gmail.com>
2024-01-17 01:56:10 +0100

    Refactor TvShowsPage component

--- app/tvshows/page.tsx
+++ app/tvshows/page.tsx
@@ -1,40 +1,38 @@
-'use client';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { fetchTvShows } from '../lib/fetchData';
-import ColumnDisplay from '../components/column-display';
 
-export default function TvShowsPage() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+type TvShowProps = {
+  id: number;
+  overview: string;
+  poster_path: string;
+  name: string;
+  vote_average: number;
+  first_air_date?: string;
+};
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await fetchTvShows();
-        setData(data.results);
-      } catch (error) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Something went wrong...</div>;
-  }
+export default async function MoviesPage() {
+  const movies = await fetchTvShows();
 
   return (
-    <div>
-      <h1>TV Shows page</h1>
-      <ColumnDisplay data={data} />
-    </div>
+    <main className="flex flex-wrap gap-4 justify-center p-4">
+      {movies.results.map((movie: TvShowProps) => (
+        <div key={movie.id} className="flex flex-col bg-secondary w-[300px]">
+          <Image
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={`${movie.name} poster`}
+            width={300}
+            height={450}
+          />
+          <div className="p-2">
+            <h2>{movie.name}</h2>
+            <p>
+              Release date: <span>{movie.first_air_date}</span> | Rating:{' '}
+              <span>{movie.vote_average}</span>
+            </p>
+            <p>{movie.overview.slice(0, 250) + '...'}</p>
+          </div>
+        </div>
+      ))}
+    </main>
   );
 }
