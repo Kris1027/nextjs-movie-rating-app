f9d6b4eede4273e4ae85957c0f1b90d81745af90
Kris1027 <obarzanek.work@gmail.com>
2024-01-18 12:31:30 +0100

    update logo text size for larger screens

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link href="/">
-      <h1 className=" text-2xl font-semibold">Kris Movie Rating App</h1>
+      <h1 className="lg:text-2xl font-semibold">Kris Movie Rating App</h1>
     </Link>
   );
 }
