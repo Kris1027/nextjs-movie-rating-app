94afd4afe42c7c48809c6c97451cacfd64e60c96
Kris1027 <obarzanek.work@gmail.com>
2024-01-20 12:02:32 +0100

    build api for rating movies

---
+++ app/lib/rateData.ts
@@ -0,0 +1,18 @@
+export default async function RateMovie(movieId: number, rating: number) {
+  const res = await fetch(
+    `http//api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem(
+      'guest_session_id'
+    )}&api_key=1f6707947cf0eca727cc2f9b144ea076`,
+    {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json;charset=utf-8',
+      },
+
+      body: `{"value":${rating}}`,
+    }
+  );
+
+  return res.json();
+}
