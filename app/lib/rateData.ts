5877d2ee6a3d6e7a9f8846013405d823030e9995
Kris1027 <obarzanek.work@gmail.com>
2024-01-20 12:04:29 +0100

    build tvshow rate api

--- app/lib/rateData.ts
+++ app/lib/rateData.ts
@@ -1,6 +1,25 @@
-export default async function RateMovie(movieId: number, rating: number) {
+export async function RateMovie(movieId: number, rating: number) {
   const res = await fetch(
     `http//api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem(
+      'guest_session_id'
+    )}&api_key=1f6707947cf0eca727cc2f9b144ea076`,
+    {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json;charset=utf-8',
+      },
+
+      body: `{"value":${rating}}`,
+    }
+  );
+
+  return res.json();
+}
+
+export async function RateTvShow(tvShowId: number, rating: number) {
+  const res = await fetch(
+    `http//api.themoviedb.org/3/tv/${tvShowId}/rating?guest_session_id=${localStorage.getItem(
       'guest_session_id'
     )}&api_key=1f6707947cf0eca727cc2f9b144ea076`,
     {
