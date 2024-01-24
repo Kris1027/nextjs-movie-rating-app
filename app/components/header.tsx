eb037602af4b0b69d8617c9e23e297335277cc7f
Kris1027 <obarzanek.work@gmail.com>
2024-01-24 11:00:37 +0100

    add menu toggle functionality and import react-icons

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,30 @@
+'use client';
 import Logo from './logo';
 import Navigation from './navigation';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoMdCloseCircleOutline } from 'react-icons/io';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="flex justify-between bg-secondary h-10 items-center p-6 max-w-[1280px] mx-auto">
       <Logo />
-      <Navigation />
+      <div onClick={handleClick}>
+        {isMenuOpen ? (
+          <RxHamburgerMenu className="sm:hidden" size={30} />
+        ) : (
+          <IoMdCloseCircleOutline className="sm:hidden" size={30} />
+        )}
+      </div>
+      <div className="hidden sm:block">
+        <Navigation />
+      </div>
     </header>
   );
 }
