baf25930ea6ae69c1540a360a73f15cb61c5223f
Kris1027 <obarzanek.work@gmail.com>
2024-02-03 19:53:13 +0100

    refactor detailed movie item component layout and styles

--- app/components/detailed-movie-item.tsx
+++ app/components/detailed-movie-item.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import RatingItem from './rating-item';
+import { totalRatingColor } from './rating-color';
 
 type DetailedMovieItemProps = {
   id: number;
@@ -34,64 +35,74 @@ type Genres = {
 };
 
 export default function DetailedMovieItem({ movie }: Movie) {
-  const ratingMovieColor = () => {
-    if (movie.vote_average >= 9) {
-      return 'text-violet-500';
-    }
-    if (movie.vote_average >= 8) {
-      return 'text-green-500';
-    }
-    if (movie.vote_average >= 7) {
-      return 'text-yellow-500';
-    }
-    if (movie.vote_average >= 6) {
-      return 'text-orange-500';
-    }
-    if (movie.vote_average < 6) {
-      return 'text-red-500';
-    }
-  };
-
   return (
-    <div className="flex flex-col md:flex-row">
-      <Image
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-        alt={`${movie.title} poster`}
-        width={500}
-        height={750}
-        className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 px-4"
-      />
-      <div className="p-4 w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:w-4/5 2xl:w-5/6">
-        <h1 className="text-3xl font-bold">{movie.title}</h1>
-        <p className={ratingMovieColor()}>{movie.vote_average.toFixed(2)}</p>
-        <p>{movie.release_date}</p>
-        <p>IMDB ID: {movie.imdb_id}</p>
+    <div className="px-4 flex flex-col flex-wrap md:flex-row gap-4">
+      <div className="w-full h-[700px] md:w-1/2 md:h-auto relative">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt={`${movie.title} poster`}
+          objectFit="contain"
+          layout="fill"
+        />
+      </div>
+      <div className="w-full md:w-1/2">
+        <div className="flex justify-between text-3xl font-bold mb-4">
+          <h1>{movie.title}</h1>
+          <p className={totalRatingColor(movie)}>
+            {movie.vote_average.toFixed(2)}
+          </p>
+        </div>
         <p>
-          Is movie for adult:{' '}
+          <span className="font-bold text-xl">Realease date: </span>
+          {movie.release_date}
+        </p>
+        <p>
+          <span className="font-bold text-xl">IMDB ID:</span> {movie.imdb_id}
+        </p>
+        <p>
+          <span className="font-bold text-xl">Is movie for adult: </span>
           {movie.adult ? (
             <span className="text-red-500">Yes</span>
           ) : (
             <span className="text-green-500">No</span>
           )}
         </p>
-        <p>Budget: {movie.budget.toLocaleString()} $</p>
-        <p className="flex gap-1">
-          Genres:
+        <p>
+          <span className="font-bold text-xl">Budget: </span>
+          {movie.budget.toLocaleString()} $
+        </p>
+        <p className="flex gap-1 items-center">
+          <span className="font-bold text-xl">Genres:</span>
           {movie.genres.map((gen) => (
             <span key={gen.id}>{gen.name},</span>
           ))}
         </p>
-        <p>Popularity: {movie.popularity}</p>
-        <p>{movie.overview}</p>
-        <p className="flex gap-1">
-          Production companies:
+        <p>
+          <span className="font-bold text-xl">Popularity: </span>
+          {movie.popularity}
+        </p>
+        <p>
+          <span className="font-bold text-xl">Revenue: </span>
+          {movie.revenue}
+        </p>
+        <p>
+          <span className="font-bold text-xl">Runtime: </span>
+          {movie.runtime}
+        </p>
+        <p>
+          <span className="font-bold text-xl">Language: </span>
+          {movie.original_language}
+        </p>
+        <p className="flex flex-col items-center">
+          <span className="font-bold text-xl">Description: </span>
+          {movie.overview}
+        </p>
+        <p className="flex flex-col items-center">
+          <span className="font-bold text-xl">Production companies:</span>
           {movie.production_companies.map((prod) => (
             <span key={prod.id}>{prod.name},</span>
           ))}
         </p>
-        <p>Revenue: {movie.revenue}</p>
-        <p>Runtime: {movie.runtime}</p>
-        <p>Language: {movie.original_language}</p>
         <RatingItem id={movie.id} type="movie" />
       </div>
     </div>
