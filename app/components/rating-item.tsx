d443a55a472c71c3e40b67403f5e23c9dc601c65
Kris1027 <obarzanek.work@gmail.com>
2024-02-18 15:06:55 +0100

    Add login check and animation to rating form

--- app/components/rating-item.tsx
+++ app/components/rating-item.tsx
@@ -4,6 +4,8 @@ import { RateMovie, RateTvShow } from '../lib/rateData';
 import { FaStar } from 'react-icons/fa';
 import Modal from './modal';
 import { useModal } from '../contexts/modal-context';
+import { useLogin } from '../contexts/login-context';
+import { motion } from 'framer-motion';
 
 export default function RatingItem({
   id,
@@ -15,6 +17,7 @@ export default function RatingItem({
   const [rating, setRating] = useState<number>(0);
   const [hover, setHover] = useState<number | null>(null);
   const { showModal, setShowModal } = useModal();
+  const { loggedIn } = useLogin();
 
   const handleSubmit = async (ratingValue: number) => {
     let response;
@@ -33,32 +36,40 @@ export default function RatingItem({
   return (
     <>
       {showModal && <Modal />}
-      <form className="flex justify-center">
-        {[...Array(10)].map((star, i) => {
-          const ratingValue = i + 1;
-          return (
-            <label key={i}>
-              <input
-                className="hidden"
-                type="radio"
-                name="rating"
-                value={ratingValue}
-                onChange={async () => {
-                  setRating(ratingValue);
-                  await handleSubmit(ratingValue);
-                }}
-              />
-              <FaStar
-                size={40}
-                color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
-                className="cursor-pointer"
-                onMouseEnter={() => setHover(ratingValue)}
-                onMouseLeave={() => setHover(null)}
-              />
-            </label>
-          );
-        })}
-      </form>
+      {loggedIn && (
+        <motion.form
+          className="flex justify-center"
+          initial={{ opacity: 0, y: '-100vh' }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          {[...Array(10)].map((star, i) => {
+            const ratingValue = i + 1;
+            return (
+              <label key={i}>
+                <input
+                  className="hidden"
+                  type="radio"
+                  name="rating"
+                  value={ratingValue}
+                  onChange={async () => {
+                    setRating(ratingValue);
+                    await handleSubmit(ratingValue);
+                  }}
+                />
+                <FaStar
+                  size={40}
+                  color={
+                    ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'
+                  }
+                  className="cursor-pointer"
+                  onMouseEnter={() => setHover(ratingValue)}
+                  onMouseLeave={() => setHover(null)}
+                />
+              </label>
+            );
+          })}
+        </motion.form>
+      )}
     </>
   );
 }
